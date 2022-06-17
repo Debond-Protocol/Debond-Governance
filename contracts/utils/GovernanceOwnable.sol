e56602519cdcc5a0c75e6bd9838a766b9a578086
DHRUV <43223201+GrandGarcon@users.noreply.github.com>
2022-06-17 12:38:52 +0200

    adding bank and exchange contracts

--- contracts/utils/GovernanceOwnable.sol
+++ contracts/utils/GovernanceOwnable.sol
@@ -7,23 +7,28 @@ import "../interfaces/IGovernanceAddressUpdatable.sol";
 
 contract GovernanceOwnable is IActivable, IGovernanceAddressUpdatable {
 
+  
+    address _debondOperator;
+    address _airdropAddress;
+    address _apmAddress;
+    address _oracleAddress;
+    address _dataAddress;
+    address _dbitAddress;
+    address _dgovAddress;
+    address _exchangeAddress;
+    address _bankAddress; 
+     
+     
     constructor(address _governanceAddress) {
-        governanceAddress = _governanceAddress;
+        _debondOperator = _governanceAddress;
         isActive = true;
     }
 
-    address governanceAddress;
-    address airdropAddress;
-    address apmAddress;
-    address oracleAddress;
-    address dataAddress;
-    address dbitAddress;
-    address dgovAddress;
-    
+
     bool isActive;
 
     modifier onlyGovernance() {
-        require(msg.sender == governanceAddress, "Governance Restriction: Not allowed");
+        require(msg.sender == _debondOperator, "Governance Restriction: Not allowed");
         _;
     }
 
@@ -38,38 +43,48 @@ contract GovernanceOwnable is IActivable, IGovernanceAddressUpdatable {
 
     function setGovernanceAddress(address _governanceAddress) external onlyGovernance {
         require(_governanceAddress != address(0), "null address given");
-        governanceAddress = _governanceAddress;
+        _debondOperator = _governanceAddress;
     }
     
     
-    function setAirdropAddress(address _airdropAddress) external onlyGovernance {
+    function setAirdropAddress(address airdropAddress) external onlyGovernance {
         require(_airdropAddress != address(0), "null address given");
-        airdropAddress = _airdropAddress;
+        _airdropAddress = airdropAddress;
     
     }
     
-    function setAPMAddress(address  _apmAddress) external onlyGovernance {
-    require(_apmAddress != address(0), "not null address" );
-    apmAddress = _apmAddress;
+    function setAPMAddress(address  apmAddress) external onlyGovernance {
+    require(apmAddress != address(0), "not null address" );
+    _apmAddress = apmAddress;
     }
     
-    function setDebondData(address  _dataAddress) external onlyGovernance {
-    require(_dataAddress != address(0), "not null address" );
-    dataAddress = _dataAddress;
+    function setDebondData(address  dataAddress) external onlyGovernance {
+    require(dataAddress != address(0), "not null address" );
+    _dataAddress = dataAddress;
     }
     
     function setOracle (address  _newOracle) external onlyGovernance {
     require(_newOracle != address(0), "not null address" );
-    dataAddress = _dataAddress;
+    _oracleAddress = _newOracle;
     }
     
-    function setDBITAddress(address _dbitAddress) external onlyGovernance {
-    require(_dbitAddress != address(0), "not null address" );
-    dbitAddress = _dbitAddress;
+    function setDBITAddress(address dbitAddress) external onlyGovernance {
+    require(dbitAddress != address(0), "not null address" );
+    _dbitAddress = dbitAddress;
     }
 
-    function  setDGOVAddress(address _dgovAddress) external onlyGovernance {
-    require( _dgovAddress != address(0), "not null address" );
-    dgovAddress =  _dgovAddress;
+    function  setDGOVAddress(address dgovAddress) external onlyGovernance {
+    require( dgovAddress != address(0), "not null address" );
+    _dgovAddress =  dgovAddress;
+    }
+
+    function  setBankAddress(address BankAddress) external onlyGovernance {
+    require( BankAddress != address(0), "not null address" );
+     _bankAddress =  BankAddress;
+    }
+
+    function  setExchangeAddress(address exchangeAddress) external onlyGovernance {
+    require( exchangeAddress != address(0), "not null address" );
+    _exchangeAddress = exchangeAddress;  
     }
 }
