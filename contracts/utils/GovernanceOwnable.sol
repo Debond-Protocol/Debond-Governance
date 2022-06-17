05650d43e2f865bed53596f7a5da09843cbbf811
DHRUV <43223201+GrandGarcon@users.noreply.github.com>
2022-06-17 11:38:46 +0200

    adding remaining address updations

--- contracts/utils/GovernanceOwnable.sol
+++ contracts/utils/GovernanceOwnable.sol
@@ -13,6 +13,13 @@ contract GovernanceOwnable is IActivable, IGovernanceAddressUpdatable {
     }
 
     address governanceAddress;
+    address airdropAddress;
+    address apmAddress;
+    address oracleAddress;
+    address dataAddress;
+    address dbitAddress;
+    address dgovAddress;
+    
     bool isActive;
 
     modifier onlyGovernance() {
@@ -32,5 +39,37 @@ contract GovernanceOwnable is IActivable, IGovernanceAddressUpdatable {
     function setGovernanceAddress(address _governanceAddress) external onlyGovernance {
         require(_governanceAddress != address(0), "null address given");
         governanceAddress = _governanceAddress;
+    }
+    
+    
+    function setAirdropAddress(address _airdropAddress) external onlyGovernance {
+        require(_airdropAddress != address(0), "null address given");
+        airdropAddress = _airdropAddress;
+    
+    }
+    
+    function setAPMAddress(address  _apmAddress) external onlyGovernance {
+    require(_apmAddress != address(0), "not null address" );
+    apmAddress = _apmAddress;
+    }
+    
+    function setDebondData(address  _dataAddress) external onlyGovernance {
+    require(_dataAddress != address(0), "not null address" );
+    dataAddress = _dataAddress;
+    }
+    
+    function setOracle (address  _newOracle) external onlyGovernance {
+    require(_newOracle != address(0), "not null address" );
+    dataAddress = _dataAddress;
+    }
+    
+    function setDBITAddress(address _dbitAddress) external onlyGovernance {
+    require(_dbitAddress != address(0), "not null address" );
+    dbitAddress = _dbitAddress;
+    }
+
+    function  setDGOVAddress(address _dgovAddress) external onlyGovernance {
+    require( _dgovAddress != address(0), "not null address" );
+    dgovAddress =  _dgovAddress;
     }
 }
