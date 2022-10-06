bcfd3fdbce6e7e128e42ba897f96a0516443c266
Edoumou <devforsay@gmail.com>
2022-10-06 10:42:56 +0200

    10-06-2022 10:42 am, Gov: check targets, values and calldata length in createProposal

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -56,6 +56,10 @@ contract Governance is GovernanceMigrator, ReentrancyGuard, IGovSharedStorage {
         string memory _title,
         bytes32 _descriptionHash
     ) public {
+        require(
+            _targets.length == _values.length && _targets.length == _calldatas.length,
+            "Gov: invalid proposal, check input data lengths"
+        );
         // proposer must have a required minimum amount of vote tokens available -to be locked-
         require(
             IERC20(
